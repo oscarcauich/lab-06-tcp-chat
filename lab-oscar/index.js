1dfa130f3c9b23d1879633da924b642c02d7423a
Oscar Cauich <ocauich@gravitypayments.com>
2017-06-19 14:44:36 -0700

    started function that initializes the server on port 3000

--- lab-oscar/index.js
+++ lab-oscar/index.js
@@ -2,3 +2,9 @@
 
 const net = require('net');
 const server = net.createServer();
+
+//define port our server should be listening to
+const PORT = 3000;
+
+server.listen(PORT, ()=>
+  console.log('server started on Port #: ', PORT));//start server and console.log the port #
